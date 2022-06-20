f4181f6724dcc5c83bae64d999a1e46d34ec07c4
KupasKode.com <72744397+kupaskode@users.noreply.github.com>
2022-06-20 08:01:00 +0800

    update javascript btn to top

--- components/atoms/BackToTop/index.tsx
+++ components/atoms/BackToTop/index.tsx
@@ -1,29 +1,35 @@
-import Link from 'next/link';
 import { useEffect } from 'react';
 
 export default function BackToTop() {
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      const toTop = document.querySelector('#to-top');
-      if (window.pageYOffset > 1000) {
+    const toTop = document.getElementById('to-top');
+
+    window.onscroll = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
         toTop?.classList.remove('hidden');
         toTop?.classList.add('flex');
       } else {
         toTop?.classList.remove('flex');
         toTop?.classList.add('hidden');
       }
-    });
+
+      toTop?.addEventListener('click', () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      });
+    };
   }, []);
 
   return (
-    <Link href="#home">
-      <a
-        type="button"
-        className="fixed bottom-4 right-4 z-[9999] hidden h-14 w-14 animate-bounce items-center justify-center rounded-full bg-primary p-4"
-        id="to-top"
-      >
-        <i className="fa-solid fa-chevron-up text-2xl text-white" />
-      </a>
-    </Link>
+    <button
+      type="button"
+      className="fixed bottom-4 right-4 z-[9999] hidden h-14 w-14 animate-bounce items-center justify-center rounded-full bg-primary p-4"
+      id="to-top"
+    >
+      <i className="fa-solid fa-chevron-up text-2xl text-white" />
+    </button>
   );
 }
